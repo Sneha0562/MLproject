9ed37dd9fe37419b79e120a158156700aed5da49
Sneha Hegde <snehabellemadk@gmail.com>
2025-04-06 00:58:25 +0530

    Initial commit

--- src/exception.py
+++ src/exception.py
@@ -13,18 +13,7 @@ class CustomException(Exception):
         line_number = exc_tb.tb_lineno
         return f"Error occurred in script: {file_name} at line {line_number} with message: {error_message}"
     
-    def __str__(self):
-        return self.error_message
-
-if __name__ == "__main__":
-    try:
-        # Intentional error: division by zero
-        a = 1 / 0
-    except Exception as e:
-        # Configure logging
-        logging.basicConfig(level=logging.INFO)
-        logging.info("Divide by zero error occurred")
-        raise CustomException(e, sys)
-
+    
+        
             
     
